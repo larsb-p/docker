551a7841ecbad9d04d7fc862e54d6f7378959751
larsb-p <lars@physik.tu-berlin.de>
2024-11-16 16:49:13 +0000

    Updated NuOscillator docker files to clone from my own fork of NuOscillator

--- alma9/NuOscillator/make_nuoscillator.sh
+++ alma9/NuOscillator/make_nuoscillator.sh
@@ -10,15 +10,15 @@ NUOSCILLATOR_VERSION=1.0.2
 docker buildx build . -t lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-x86_64 \
                        --platform=linux/amd64 \
                        --load --ssh default --build-arg NUOSCILLATOR_VERSION=${NUOSCILLATOR_VERSION} --build-arg NCORES=8
-##docker push lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9-x86_64
+docker push lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-x86_64
 
-#docker buildx build . -t lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9-aarch64 \
-#                        --platform=linux/arm64 \
-#                        --load --ssh default --build-arg NUOSCILLATOR_VERSION=${NUOSCILLATOR_VERSION} --build-arg NCORES=8
-#docker push lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9-aarch64
+docker buildx build . -t lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-aarch64 \
+                        --platform=linux/arm64 \
+                        --load --ssh default --build-arg NUOSCILLATOR_VERSION=${NUOSCILLATOR_VERSION} --build-arg NCORES=8
+docker push lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-aarch64
 
-#docker manifest rm lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9 || true
-#docker manifest create lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9 \
-#                --amend lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9-aarch64 \
-#                --amend lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9-x86_64
-#docker manifest push lbathepeters/nusquids_${NUOSCILLATOR_VERSION}:alma9
+docker manifest rm lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9 || true
+docker manifest create lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9 \
+                --amend lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-aarch64 \
+                --amend lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9-x86_64
+docker manifest push lbathepeters/nuoscillator_${NUOSCILLATOR_VERSION}:alma9
